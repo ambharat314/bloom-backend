0a41b0edca88a4a70e642347c2a9ef41c2fe2f5d
ambharat314 <ambharat314@gmail.com>
2026-05-10 21:11:50 +0530

    Update supabaseAdmin.js

--- src/supabaseAdmin.js
+++ src/supabaseAdmin.js
@@ -1,8 +1,15 @@
+require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
+const ws = require('ws');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY  // service_role bypasses RLS
+  process.env.SUPABASE_SERVICE_KEY,
+  {
+    realtime: {
+      transport: ws
+    }
+  }
 );
 
 module.exports = supabase;
